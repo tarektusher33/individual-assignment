dcab67a9f2f0292f34e53b1ae1c2d9bb72f7978b
tarektusher33 <tarek.tusher@brainstation-23.com>
2024-04-25 11:23:47 +0600

    add fibo.cpp file for rebasing

---
+++ fibo.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a = 1 , b = 1;
+    for(int i=2;i<=5;i++){
+        int temp = a+b;
+        a = b;
+        b = temp;
+    }
+    cout<<b<<endl;
+return 0;
+}
